c84db884c41a25dd0e5e671083efeb6dd68a003f
joey <joey_qpb@hotmail.com>
2024-02-23 22:56:10 +0100

    done

--- src/test/java/com/joey/emailservice/EmailServiceApplicationTests.java
+++ src/test/java/com/joey/emailservice/EmailServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.joey.emailservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import emailservice.EmailServiceApplication;
+
+@SpringBootTest(classes = EmailServiceApplication.class)
 class EmailServiceApplicationTests {
 
 	@Test
